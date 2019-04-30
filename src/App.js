15672d450100adfa7d5ebb721541f359af52d79c
Matt Raines <mraines4@gmail.com>
2019-04-30 15:21:40 -0400

    shows how to setup router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import Dogs from './Dogs';
+
+import {
+  Link,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* link is what you use instead of anchor */}
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/dogs">Dogs</Link>
+        {/* <Home /> */}
+        <Route exact path="/" component={Home} />
+        {/* <About /> */}
+        <Route path="/about" component={About} />
+        {/* <Dogs />  */}
+        <Route path="/dogs" component={Dogs} />
       </header>
     </div>
   );
---
+++ src/Dogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dogs(props) {
+    return (
+        <div>
+            <h1>🐶</h1>
+        </div>
+    )
+}
+
+export default Dogs;
