892ea97a3b728a69cf2c86116ea6b3d5d568c9de
Matt Raines <mraines4@gmail.com>
2019-05-01 10:29:55 -0400

    bind demo

--- src/App.js
+++ src/App.js
@@ -10,24 +10,51 @@ import {
   Route
 } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* link is what you use instead of anchor */}
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/dogs">Dogs</Link>
-        {/* <Home /> */}
-        <Route exact path="/" component={Home} />
-        {/* <About /> */}
-        <Route path="/about" component={About} />
-        {/* <Dogs />  */}
-        <Route path="/dogs" component={Dogs} />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+    this._incrementCount = this._incrementCount.bind(this)
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>{this.state.counter}</h1>
+          <button onClick={this._incrementCountUsingArrow}>increment</button>
+          {/* link is what you use instead of anchor */}
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/dogs">Dogs</Link>
+          {/* <Home /> */}
+          <Route exact path="/" component={Home} />
+          {/* <About /> */}
+          <Route path="/about" component={About} />
+          {/* <Dogs />  */}
+          <Route path="/dogs" component={Dogs} />
+        </header>
+      </div>
+    );
+  }
+
+  _incrementCount() {
+    console.log('app: increment count ');
+    console.log(this);
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+  _incrementCountUsingArrow = () => {
+    console.log('app: increment count using arrow');
+    console.log(this);
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
 }
 
 export default App;
